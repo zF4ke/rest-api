3f47fe53ffbb032c3d3a8cf74c518e91fde0a626
zF4ke <pedrohsilva955@gmail.com>
2022-03-31 16:44:25 +0100

    Body Parser e CORS

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 import 'dotenv/config'
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 /* App */
 const app = express()
 
+/* Logging */
 app.use(morgan('dev'))
+
+/* Body Parser */
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+/* CORS */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
+})
 
 /* Routes */
 import peopleRoute from "./api/routes/people.js"
