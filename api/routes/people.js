8d10d2bf0456057db05b85dbacd174ec2dc3712d
zF4ke <pedrohsilva955@gmail.com>
2022-03-31 19:39:39 +0100

    Type Checking e Metodos

--- api/routes/people.js
+++ api/routes/people.js
@@ -1,22 +1,51 @@
+import check from 'check-types'
 import express from 'express';
 const router = express.Router()
 
+import Person from '../models/person.js'
+
 /* /people */
 
-router.get('/', (req, res) => {
-    return res.status(200).json({
-        message: 'Getting all people'
-    })
+router.get('/', async (req, res) => {
+    const people = await Person.find()
+
+    return res.status(200).json(people)
 })
 
 /*
  * @param {array} fields
  */
 
-router.post('/', (req, res) => {
-    return res.status(200).json({
-        message: 'Creating a new person'
+router.post('/', async (req, res) => {
+    const fields = req.body?.fields
+
+    if (!check.assigned(fields)) return res.status(400).json({
+        message: 'Missing fields'
     })
+
+    if (!check.array.of.object(fields)) return res.status(400).json({
+        message: 'Fields must be an array of objects'
+    })
+
+    if (check.all(check.map(fields, field => check.emptyObject(field)))) return res.status(400).json({
+        message: 'Fields array must not be empty'
+    })
+
+    try {
+        const person = new Person({
+            fields: req.body.fields
+        })
+
+        await person.save()
+
+        return res.status(200).json({
+            message: 'Person created successfully.',
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
 })
 
 /* /people/:id */
@@ -25,10 +54,27 @@ router.post('/', (req, res) => {
  * @param {id} id
  */
 
-router.get('/:id', (req, res) => {
-    return res.status(200).json({
-        message: `Getting a single person with id ${req.params.id}`
+router.get('/:id', async (req, res) => {
+    const id = req.params?.id
+
+    if (check.emptyString(id)) return res.status(400).json({
+        message: 'Missing id'
     })
+
+    if (!check.hasLength(id, 24)) return res.status(400).json({
+        message: 'Invalid id'
+    })
+
+    try {
+        const person = await Person.findById(id)
+
+        return res.status(200).json(person)
+
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
 })
 
 /*
@@ -45,10 +91,33 @@ router.patch('/:id', (req, res) => {
  * @param {id} id
  */
 
-router.delete('/:id', (req, res) => {
-    return res.status(200).json({
-        message: `Deleting a single person with id ${req.params.id}`
+router.delete('/:id', async (req, res) => {
+    const id = req.params?.id
+
+    if (check.emptyString(id)) return res.status(400).json({
+        message: 'Missing id'
     })
+
+    if (!check.hasLength(id, 24)) return res.status(400).json({
+        message: 'Invalid id'
+    })
+
+    try {
+        const person = await Person.findByIdAndDelete(id)
+
+        if (check.assigned(person)) return res.status(200).json({
+            message: 'Person deleted successfully'
+        })
+
+        return res.status(404).json({
+            message: 'Person not found'
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
 })
 
 export default router
