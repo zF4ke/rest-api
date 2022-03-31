4621bddc39b0e93b2f0d8da78fd5e2604003f6b7
zF4ke <pedrohsilva955@gmail.com>
2022-03-31 16:15:34 +0100

    Tratamento de erros

--- app.js
+++ app.js
@@ -14,5 +14,21 @@ import usersRoute from "./api/routes/users.js"
 app.use('/people', peopleRoute)
 app.use('/users', usersRoute)
 
+/* Handle 404 error */
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+/* Handle 5xx error */
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
