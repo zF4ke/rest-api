121ab6bb15422bd57320477a884e0212cc0ff042
zF4ke <pedrohsilva955@gmail.com>
2022-04-02 14:46:00 +0100

    Modelo User

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
