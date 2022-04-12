e3a22945cfb8982898d61ed5abf7a834d096135f
zF4ke <pedrohsilva955@gmail.com>
2022-04-12 13:30:30 +0100

    Rota de Login

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router()
 import bcrypt from 'bcrypt';
 import check from 'check-types'
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 import User from '../models/user.js'
 
@@ -47,6 +47,53 @@ router.post('/signup', validateUser, async (req, res) => {
         res.status(201).json({
             message: 'User created successfully.',
             user
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const username = req.body?.username
+    const password = req.body?.password
+
+    if (!check.assigned(username)) return res.status(400).json({
+        message: 'Missing username'
+    })
+
+    if (!check.assigned(password)) return res.status(400).json({
+        message: 'Missing password'
+    })
+
+    try {
+
+        // Check if user exists
+
+        const user = await User.findOne({ username })
+        if (!user) return res.status(401).json({
+            message: 'Invalid username or password'
+        })
+
+        // Check if password is correct
+
+        const isValid = bcrypt.compareSync(password, user.password)
+        if (!isValid) return res.status(401).json({
+            message: 'Invalid username or password'
+        })
+
+        // Create token
+
+        const token = jwt.sign({
+            id: user._id,
+            username: user.username
+        }, process.env.JWT_SECRET)
+
+        res.status(200).json({
+            message: 'Login successful',
+            token
         })
     } catch (err) {
         console.error(err)
